1859efad6ed0a8d43c81dc23d79ca219e84e9084
hoream <2175223953@qq.com>
2022-07-06 09:50:23 +0800

    config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10",
-                                        "WenQuanYi Micro Hei:size=10:type=Regular:antialias=true:autohint=true", 
+static const char *fonts[]          = { "monospace:size=14",
+                                        "WenQuanYi Micro Hei:size=14:type=Regular:antialias=true:autohint=true", 
                                         "Symbols Nerd Font:pixelsize=16:type=2048-em:antialias=true:autohint=true" };
 //static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
